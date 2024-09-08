820e3e63fb334de65aa42b3e792a0771c7ace916
Ryan Grunest <ryangrunest@geeko>
2024-09-08 15:17:52 -0700

    init

---
+++ m1/main.cpp
@@ -0,0 +1,11 @@
+// Ryan Grunest
+// September 8th, 2024
+// Purpose of program: Print Hello World to console
+
+#include <iostream>
+
+using namespace std;
+int main( int argc, char *argv[], char *envp[] )
+{
+  cout << "Hello, World!" << endl; // prints "Hello, World!"
+}
---
+++ m1/main.py
@@ -0,0 +1,5 @@
+# Ryan Grunest
+# September 8th, 2024
+# Purpose of program: Print Hello World to console
+
+print("Hello, World!") # Prints "Hello, World!"
